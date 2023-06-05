151e61d22cff392f26af398bfacd260e72ceee25
user45674 <ck56coder@gmail.com>
2023-06-05 10:27:57 +0530

    Endpoint for adding notes

--- src/main/java/com/note_share_res_api/backend_rest_api/controllers/NoteController.java
+++ src/main/java/com/note_share_res_api/backend_rest_api/controllers/NoteController.java
@@ -1,43 +1,38 @@
 package com.note_share_res_api.backend_rest_api.controllers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.note_share_res_api.backend_rest_api.modesl.Note;
-import com.note_share_res_api.backend_rest_api.repository.NoteRepository;
+import java.util.*;
+import org.springframework.data.domain.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import com.note_share_res_api.backend_rest_api.modesl.*;
+import com.note_share_res_api.backend_rest_api.repository.*;
 
 @RestController
 public class NoteController {
-     
+
     NoteRepository noteService;
-    NoteController(NoteRepository noteService){
+    UserRepository userService;
+
+    NoteController(NoteRepository noteService, UserRepository userService) {
         this.noteService = noteService;
+        this.userService = userService;
     }
-    @GetMapping(value="/notes")
-    public ResponseEntity<Map<String, Object>> getAllNotes( 
-        @RequestParam(required = false,defaultValue = "-1") int ownerId,
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "3") int size
-    ) {
-        try{
+
+    @GetMapping(value = "/notes")
+    public ResponseEntity<Map<String, Object>> getAllNotes(
+            @RequestParam(required = false, defaultValue = "-1") int ownerId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "3") int size) {
+        try {
 
             List<Note> notes = new ArrayList<Note>();
             Pageable pagingSort = PageRequest.of(page, size);
 
             Page<Note> notesPage;
-            if(ownerId == -1)  notesPage = noteService.findAll(pagingSort);
-            else notesPage = noteService.findByOwnerId(ownerId,pagingSort);
+            if (ownerId == -1)
+                notesPage = noteService.findAll(pagingSort);
+            else
+                notesPage = noteService.findByOwnerId(ownerId, pagingSort);
 
             notes = notesPage.getContent();
 
@@ -47,11 +42,18 @@ public class NoteController {
             response.put("totalItems", notesPage.getTotalElements());
             response.put("totalPages", notesPage.getTotalPages());
 
-
             return new ResponseEntity<>(response, HttpStatus.OK);
-        }   catch (Exception e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-          }
+        }
     }
+
     
+    @PostMapping(value = "/notes/{id}")
+    public Note addNote(@PathVariable int id, @RequestBody Note noteData) {
+        Optional<User> user = userService.findById(id);
+        noteData.setUser(user.get());
+        Note note = noteService.save(noteData);
+        return note;
+    }
 }
