8df141690e89912212771642449b141b941e4a5d
user45674 <ck56coder@gmail.com>
2023-06-05 11:06:15 +0530

    delete Note endpoint

--- src/main/java/com/note_share_res_api/backend_rest_api/controllers/NoteController.java
+++ src/main/java/com/note_share_res_api/backend_rest_api/controllers/NoteController.java
@@ -50,10 +50,40 @@ public class NoteController {
 
     
     @PostMapping(value = "/notes/{id}")
-    public Note addNote(@PathVariable int id, @RequestBody Note noteData) {
-        Optional<User> user = userService.findById(id);
-        noteData.setUser(user.get());
-        Note note = noteService.save(noteData);
-        return note;
+    public ResponseEntity<Note> addNote(@PathVariable int id, @RequestBody Note noteData) {
+        try{
+            Optional<User> user = userService.findById(id);
+            noteData.setUser(user.get());
+            Note note = noteService.save(noteData);
+            return new ResponseEntity<>(note, HttpStatus.CREATED);
+        }catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PatchMapping(value = "/notes/{postId}")
+    public ResponseEntity<String> updatePost(@PathVariable int postId, @RequestBody Note noteData) {
+        try{
+            
+            //update post with changes that occurs
+            return new ResponseEntity<>("will do it later!!", HttpStatus.CREATED);
+        }catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping(value = "notes/{postId}")
+    public ResponseEntity<String> deleteNote(@PathVariable int postId){
+        try{
+            Optional<Note> note = noteService.findById(postId);
+            if(note.isEmpty()){
+                return new ResponseEntity<>("Post doesn't exits", HttpStatus.BAD_REQUEST);
+            }
+            
+            noteService.deleteById(postId);
+            return new ResponseEntity<>("Sucessfully Deleted Note with id" + postId, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+           
     }
 }
